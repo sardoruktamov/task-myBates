6e92323d14838ee2e5a0b55445cceda4c86a2df8
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-02-22 23:44:40 +0500

    create project

---
+++ taskmybates/src/main/java/uz/javadev/taskmybates/TaskmybatesApplication.java
@@ -0,0 +1,13 @@
+package uz.javadev.taskmybates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmybatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmybatesApplication.class, args);
+	}
+
+}
---
+++ taskmybates/src/test/java/uz/javadev/taskmybates/TaskmybatesApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.javadev.taskmybates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmybatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
